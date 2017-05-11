0a6e04ff688441e41d29a70489518a41cf35c829
miyuh <miyu0706h@gmail.com>
2017-05-11 21:29:07 +0900

    add

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from flask import render_template,request
 
 app = Flask(__name__)
 
+system =  ["システム","青山"]
+
 @app.route("/")
 def index():
     return render_template('index.html')
@@ -12,10 +14,14 @@ def index():
 def add():
     user = request.form["user"]
     message = request.form["message"]
-    if message == "システム創成学":
-        url = "http://www.sys.t.u-tokyo.ac.jp/admissions/" #この""は文字列と認識するためのもの
-    elif message == "技術経営戦略学":
-        url = "http://tmi.t.u-tokyo.ac.jp/examinfo/examinfo.htm"
+    for i in (0,1):
+        if system[i] in message:
+            url = "http://www.sys.t.u-tokyo.ac.jp/admissions/" #この""は文字列と認識するためのもの
+            break
+
+        elif message == "技術経営戦略学":
+            url = "http://tmi.t.u-tokyo.ac.jp/examinfo/examinfo.htm"
+
     return render_template('page1.html', user=user, message = message, url = url) #
 
 if __name__ == "__main__":
