09b9adb77afd63e91f2ec9876b8e8a1bf60a1657
Shintaro-Indo <shintaro0724baseball@gmail.com>
2017-05-11 18:18:57 +0900

    TMIを追加

--- main.py
+++ main.py
@@ -12,7 +12,11 @@ def index():
 def add():
     user = request.form["user"]
     message = request.form["message"]
-    return render_template('page1.html', user=user, message = message)
+    if message == "システム創成学":
+        url = "http://www.sys.t.u-tokyo.ac.jp/admissions/" #この""は文字列と認識するためのもの
+    elif message == "技術経営戦略学":
+        url = "http://tmi.t.u-tokyo.ac.jp/examinfo/examinfo.htm"
+    return render_template('page1.html', user=user, message = message, url = url) #
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
