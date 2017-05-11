13647f3df94d2493808608604d1e5d169b13b2a3
Shintaro-Indo <shintaro0724baseball@gmail.com>
2017-05-11 18:00:06 +0900

    青山

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template,request
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('index.html', message="世界")
+    return render_template('index.html')
+
+@app.route("/add",methods=['POST'])
+def add():
+    user = request.form["user"]
+    message = request.form["message"]
+    return render_template('page1.html', user=user, message = message)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
